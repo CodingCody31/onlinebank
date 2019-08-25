1875d02776f086bc9cbadd347db0d470ec9d0699
Sunzidabeg <sunzidabeg01@gmail.com>
2019-08-25 16:55:53 -0500

    test commit

--- src/main/java/com/meritamerica/onlinebank/controllers/OnlineBank.java
+++ src/main/java/com/meritamerica/onlinebank/controllers/OnlineBank.java
@@ -1,5 +1,31 @@
 package com.meritamerica.onlinebank.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
 public class OnlineBank {
 
+	
+@RequestMapping("/home")
+    
+	
+
+
+@RequestMapping("/account")
+
+
+
+@RequestMapping("/transaction")
+
+
+
+@RequestMapping("/setting")
+
+
+
+@RequestMapping("/transfers")
+
+
 }
+	
+
