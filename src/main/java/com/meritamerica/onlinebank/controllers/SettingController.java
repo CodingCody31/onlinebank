bcb27316ee4622945af96e4d4a8bb388b582f824
Sunzidabeg <sunzidabeg01@gmail.com>
2019-09-08 10:30:39 -0500

    please check

--- src/main/java/com/meritamerica/onlinebank/controllers/SettingController.java
+++ src/main/java/com/meritamerica/onlinebank/controllers/SettingController.java
@@ -1,13 +1,27 @@
 package com.meritamerica.onlinebank.controllers;
 
+import java.util.List;
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.meritamerica.onlinebank.models.User;
+import com.meritamerica.onlinebank.services.SettingService;
 
 @Controller
 public class SettingController {
-	@RequestMapping("/settings")
-	public String settings(){
-	return "/Setting.jsp";
-	}
+	private final SettingService settingService;
 
-}
+	public SettingController(SettingService settingService) {
+	     this.settingService = settingService;
+	 }
+	 
+
+	@RequestMapping("/useraccount/new")
+	  public String newUser(@ModelAttribute("user") User user) {
+	      return "/users/setting.jsp";
+	  }
+	
+	
+	}
--- src/main/java/com/meritamerica/onlinebank/repositories/AccountRepository.java
+++ src/main/java/com/meritamerica/onlinebank/repositories/AccountRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface AccountRepository{
 
+	void deleteById(Long id);
+
 }
