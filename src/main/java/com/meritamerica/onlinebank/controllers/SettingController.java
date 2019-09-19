84de06bf41963802fa81b35124218deab615dd6a
Sunzidabeg <sunzidabeg01@gmail.com>
2019-09-18 21:15:53 -0500

    test commit

--- src/main/java/com/meritamerica/onlinebank/controllers/SettingController.java
+++ src/main/java/com/meritamerica/onlinebank/controllers/SettingController.java
@@ -1,67 +1,59 @@
 package com.meritamerica.onlinebank.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.meritamerica.onlinebank.models.User;
 import com.meritamerica.onlinebank.services.SettingService;
+import com.meritamerica.onlinebank.services.UserService;
 
 
 
 @Controller
 public class SettingController {
+	private final UserService userService;
 	private final SettingService settingService;
 
-	public SettingController(SettingService settingService) {
+	public SettingController(SettingService settingService, UserService userService) {
 	     this.settingService = settingService;
+	     this.userService = userService;
 	 }
 	
-	
-	@RequestMapping("/useraccount/new")
-	  public String newUser(@ModelAttribute("user") User user) {
-	      return "/users/setting.jsp";
-	  }
-
-
-	
-public String createaccount(@Valid @ModelAttribute("user") User user, BindingResult result) {
-	System.out.println("hit route correctly");
-	if(result.hasErrors()) {
-		System.out.println("escaped due to errors");
-		return "newaccount";			
-	}
-	System.out.println("creating account");
-	settingService.addAccount(user);
-	return "redirect:/accounts";
-}
-	
-
-//create account
-	@RequestMapping(value="/account/new", method=RequestMethod.POST)
-	public String addaccount(@Valid @ModelAttribute("user") User user, BindingResult result) {
-		System.out.println("hit route correctly");
-		if(result.hasErrors()) {
-			System.out.println("escaped due to errors");
-			return "newaccount";			
-		}
-		System.out.println("creating account");
-		settingService.addAccount(user);
-		return "redirect:/accounts";
-
-
-
+	@RequestMapping("/settings/{id}")
+	public String settings(@PathVariable("id") Long id, Model model) {
+		User user = userService.findUsers(id);
+		model.addAttribute("user", user);
+		return "/Setting.jsp";
 	}
 	
-	@RequestMapping("/update") 
-	 public String updateUser(long id, String email, String name) {
-	    return "User successfully updated";
-	  }	
+	@RequestMapping("/update/{id}")
+	public String updateuser(@PathVariable("id") Long id, Model model) {
+		User user = userService.findUsers(id);
+		model.addAttribute("user", user);
+		return "/UserUpdate.jsp";
+	}
+
 	
+	
+	
+	@RequestMapping(value="/update/{id}", method=RequestMethod.PUT)
+	   public String updateuser(@Valid @ModelAttribute("user") User user, BindingResult result) {
+	       if (result.hasErrors()) {
+	           return "/UserUpdate.jsp";
+	       } else {
+	           settingService.updateUser(user);
+	           return "redirect:/Setting.jsp";
+	       }
+	   }
 	
 	@RequestMapping("/delete")
 	  	  public String delete(long id) {
