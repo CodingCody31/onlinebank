de3cb74e7620833efbca044e2ffef9cf2cc74f2d
Cody Ortiz <codyo990@yahoo.com>
2019-09-18 22:10:57 -0500

    update

--- src/main/java/com/meritamerica/onlinebank/repositories/AccountRepository.java
+++ src/main/java/com/meritamerica/onlinebank/repositories/AccountRepository.java
@@ -13,15 +13,9 @@ import com.meritamerica.onlinebank.models.User;
 @Repository
 public interface AccountRepository extends CrudRepository <Account, Long> {
 	
-
-<<<<<<< HEAD
-public interface AccountRepository extends CrudRepository {
-=======
 	List<Account> findAll();
->>>>>>> c352c8d7bae135f8e0051fdd9b93c5820f203e80
 
-	
-	
+
 
 }
 
